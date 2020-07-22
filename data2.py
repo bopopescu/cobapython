997e37b1526f3519d21e567e44c37a5c2a5485a0
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 02:45:07 +0300

    Discarding the master and slave phrasing

--- data2.py
+++ data2.py
@@ -12,7 +12,7 @@ try:
     with conn.cursor() as cursor:
         # Create a new record
         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+        cursor.execute(sql, ('webmain@python.org', 'very-secret'))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
@@ -21,7 +21,7 @@ try:
     with conn.cursor() as cursor:
         # Read a single record
         sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('webmaster@python.org',))
+        cursor.execute(sql, ('webmain@python.org',))
         result = cursor.fetchone()
         print(result)
 finally:
